eb4a01bc679a5ba316bf18693bcc2979b46116ac
unknown <wadiesoyass@gmail.com>
2021-04-15 10:41:24 +0400

    autentication done

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {AngularFireAuth} from '@angular/fire/auth';
+import {Observable} from 'rxjs';
+import {first, map} from 'rxjs/operators';
+import firebase from 'firebase';
+import {loggedIn} from '@angular/fire/auth-guard';
 
 @Component({
   selector: 'app-home',
@@ -7,14 +11,19 @@ import {AngularFireAuth} from '@angular/fire/auth';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  isLoggedIn = new Observable();
+  isLoggedout = new Observable();
 
   constructor(private afAuth: AngularFireAuth) {
   }
 
   ngOnInit(): void {
     this.afAuth.authState.subscribe(user => console.log(user));
+    this.isLoggedIn = this.afAuth.authState.pipe(first());
+    this.isLoggedout = this.afAuth.authState.pipe(map(loggedIn => !loggedIn));
   }
-  logOut(){
+
+  logOut() {
     this.afAuth.signOut();
   }
 
